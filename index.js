4d8ad8d24d782016dc3172837b5dcc81a156f0a9
ChaseDev7 <chasewebdev101@gmail.com>
2023-10-09 21:01:29 -0700

    Update js

--- index.js
+++ index.js
@@ -112,4 +112,37 @@ const LinkedList = () => {
     nodeBeforeIndex.nextNode = nodeBeforeIndex.nextNode.nextNode;
     size--;
   };
+
+  return {
+    append,
+    prepend,
+    get size(){ return size; },
+    get head(){ return head; },
+    at,
+    tail,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+  };
 };
+
+const list = LinkedList();
+list.append(5);
+list.append(6);
+list.append(7)
+list.prepend(10);
+list.prepend(9);
+list.prepend(8);
+list.insertAt(3, 1);
+list.pop();
+list.removeAt(4);
+
+console.log(list.size);
+console.log(list.head);
+console.log(list.at(1));
+console.log(list.find(10));
+console.log(list.tail());
+console.log(list.toString());
