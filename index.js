f1de519b8c2a2ff5c86933f03b331dcf52b57703
ChaseDev7 <chasewebdev101@gmail.com>
2023-10-09 21:14:21 -0700

    Update commands in js

--- index.js
+++ index.js
@@ -130,19 +130,30 @@ const LinkedList = () => {
 };
 
 const list = LinkedList();
-list.append(5);
-list.append(6);
-list.append(7)
-list.prepend(10);
-list.prepend(9);
-list.prepend(8);
-list.insertAt(3, 1);
-list.pop();
-list.removeAt(4);
 
-console.log(list.size);
-console.log(list.head);
-console.log(list.at(1));
-console.log(list.find(10));
-console.log(list.tail());
 console.log(list.toString());
+list.append(20);
+console.log(list.toString());
+list.append(3);
+console.log(list.toString());
+list.append(7);
+console.log(list.toString());
+list.prepend(14);
+console.log(list.toString());
+list.prepend(21);
+console.log(list.toString());
+list.prepend(32);
+console.log(list.toString());
+list.insertAt(3, 1);
+console.log(list.toString());
+list.append(25);
+console.log(list.toString());
+list.pop();
+console.log(list.toString());
+list.removeAt(4);
+console.log(list.toString());
+console.log(list.head);
+console.log(list.tail());
+console.log(list.at(3));
+console.log(list.find(7));
+console.log(list.size);
